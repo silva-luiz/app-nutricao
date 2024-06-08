c3ac847a2bbcbdc5a6cadabaf306d5bd2943f94a
Luiz Henrique Gomes da Silva <luiz.hgsilva@hotmail.com>
2024-06-08 14:48:01 -0300

    routes changed

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,17 +1,19 @@
+import 'package:app_nutricao/pages/add_page.dart';
+import 'package:app_nutricao/pages/index.dart';
 import 'package:app_nutricao/pages/login.dart';
 import 'package:app_nutricao/pages/initial_home.dart';
-import 'package:app_nutricao/pages/home.dart';
 import 'package:app_nutricao/pages/new_food.dart';
 import 'package:app_nutricao/pages/new_menu.dart';
-import 'package:app_nutricao/page/search.dart';
+import 'package:app_nutricao/pages/search.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
   static Map<String, Widget Function(BuildContext)> loadRoutes() {
     return {
+      '/home': (context) => const Index(),
       '/login': (context) => const LoginPage(),
       '/initial_home': (context) => const InitialHomePage(),
-      '/home': (context) => const HomePage(),
+      '/add_page':(context) => const AddPage(),
       '/new_food': (context) => const NewFoodPage(),
       '/new_menu': (context) => const NewMenuPage(),
       '/search': (context) => const SearchPage(),
